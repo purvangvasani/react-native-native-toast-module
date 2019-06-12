fad1cfbcc5d73720500a1326af8c173fed00a8ac
purvangvasani <purvang.vasani@aspiresoftserv.com>
2019-06-12 18:45:39 +0530

    First Commit

---
+++ App.js
@@ -0,0 +1,48 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Button, StyleSheet, Text, View} from 'react-native';
+
+import ToastExample from './ToastExample';
+
+export default class App extends Component{
+  
+  showToast = () => {
+    ToastExample.show('Awesome', ToastExample.LONG);
+  }
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Button title="Toast" onPress={this.showToast} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
